3698d5316d45cceb734d3b274fcca598cf71a705
LorDClockaN <davor@losinj.com>
2015-01-14 16:38:25 -0200

    Add custom option for DPI selection
    PS2: only numbers for EditText
    
    Change-Id: I1af75f276ea4691acd06cb3ed2089d3d828e390c
    (cherry picked from commit 38fe0e445c7830ab05af73b224dc0bd17771fd86)
    
    Conflicts:
    	src/com/android/settings/temasek/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -32,11 +32,14 @@ import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT;
 
 import android.app.Activity;
 import android.app.ActivityManagerNative;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.IActivityManager;
 import android.app.Dialog;
 import android.app.admin.DevicePolicyManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
@@ -61,9 +64,13 @@ import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,6 +87,8 @@ import org.cyanogenmod.hardware.TapToWake;
 public class DisplaySettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, OnPreferenceClickListener, Indexable {
     private static final String TAG = "DisplaySettings";
+
+    private static final int DIALOG_DENSITY = 101;
 
     /** If there is no setting in the provider, use this. */
     private static final int FALLBACK_SCREEN_TIMEOUT_VALUE = 30000;
@@ -125,6 +134,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mSunlightEnhancement;
     private SwitchPreference mColorEnhancement;
 
+    protected Context mContext;
+
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
         @Override
@@ -167,13 +178,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         updateTimeoutPreferenceDescription(currentTimeout);
         updateDisplayRotationPreferenceDescription();
 
+        mContext = getActivity().getApplicationContext();
+        int newDensityValue;
+
         mLcdDensityPreference = (ListPreference) findPreference(KEY_LCD_DENSITY);
         int defaultDensity = DisplayMetrics.DENSITY_DEVICE;
-        String[] densityEntries = new String[8];
+        String[] densityEntries = new String[9];
         for (int idx = 0; idx < 8; ++idx) {
             int pct = (75 + idx*5);
             densityEntries[idx] = Integer.toString(defaultDensity * pct / 100);
         }
+        densityEntries[8] = getString(R.string.custom_density);
         int currentDensity = DisplayMetrics.DENSITY_CURRENT;
         mLcdDensityPreference.setEntries(densityEntries);
         mLcdDensityPreference.setEntryValues(densityEntries);
@@ -570,9 +585,14 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             }
         }
         if (KEY_LCD_DENSITY.equals(key)) {
-            int value = Integer.parseInt((String) objValue);
-            writeLcdDensityPreference(value);
-            updateLcdDensityPreferenceDescription(value);
+            String strValue = (String) objValue;
+            if (strValue.equals(getResources().getString(R.string.custom_density))) {
+                showDialog(DIALOG_DENSITY);
+            } else {
+                int value = Integer.parseInt((String) objValue);
+                writeLcdDensityPreference(value);
+                updateLcdDensityPreferenceDescription(value);
+            }
         }
         if (KEY_FONT_SIZE.equals(key)) {
             writeFontSizePreference(objValue);
@@ -763,6 +783,52 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         catch (RemoteException e) {
             Log.e(TAG, "Failed to restart");
         }
+    }
+
+    public Dialog onCreateDialog(int dialogId) {
+        LayoutInflater factory = LayoutInflater.from(mContext);
+
+        switch (dialogId) {
+            case DIALOG_DENSITY:
+                final View textEntryView = factory.inflate(
+                        R.layout.alert_dialog_text_entry, null);
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.custom_density_dialog_title)
+                        .setMessage(getResources().getString(R.string.custom_density_dialog_summary))
+                        .setView(textEntryView)
+                        .setPositiveButton(getResources().getString(R.string.set_custom_density_set), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText dpi = (EditText) textEntryView.findViewById(R.id.dpi_edit);
+                                Editable text = dpi.getText();
+                                Log.i(TAG, text.toString());
+                                String editText = dpi.getText().toString();
+
+                                try {
+                                    SystemProperties.set("persist.sys.lcd_density", editText);
+                                }
+                                catch (Exception e) {
+                                    Log.w(TAG, "Unable to save LCD density");
+                                }
+                                try {
+                                    final IActivityManager am = ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+                                    if (am != null) {
+                                        am.restart();
+                                    }
+                                }
+                                catch (RemoteException e) {
+                                    Log.e(TAG, "Failed to restart");
+                                }
+                            }
+
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                                dialog.dismiss();
+                            }
+                        }).create();
+        }
+        return null;
     }
 
 }
